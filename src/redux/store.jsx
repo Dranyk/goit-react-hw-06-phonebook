a7027464daafbd9a617f916080c201dba835128a
Dranyk <dvdranyk@gmail.com>
2023-03-01 17:19:26 +0200

    Update store.jsx

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,4 +1,4 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
 import {
   persistStore,
