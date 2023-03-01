989c44c6d82fb797a1c991cd4b90150e9d3821cb
Dranyk <dvdranyk@gmail.com>
2023-03-01 18:52:59 +0200

    Update selectors.jsx

--- src/redux/selectors.jsx
+++ src/redux/selectors.jsx
@@ -1,16 +1,3 @@
-export const getContacts = store => store.contacts;
+export const getContacts = state => state.contacts;
 
-export const getFilterContacts  = ({ contacts, filter }) => {
-  if (!filter) {
-    return contacts;
-  }
-  const normalizedFilter =  filter.toLowerCase();
-  const result = contacts.filter(({ name, number }) => {
-    return (name.toLowerCase().includes(normalizedFilter) ||
-      number.includes(normalizedFilter))
-  });
-
-  return result;
-};
-
-export const getFilter = ({ filter }) => filter;
+export const getFilter = state => state.filter;
